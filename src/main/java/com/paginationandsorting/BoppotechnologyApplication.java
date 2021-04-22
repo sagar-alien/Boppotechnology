40e516b2dd0bf5e57bf8a520c10f2b405dbcb6c0
lenovo <lenovo@DESKTOP-O4HBF1G>
2021-04-22 20:12:23 +0530

    WH-8520 - First Time File Update

---
+++ src/main/java/com/paginationandsorting/BoppotechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.paginationandsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoppotechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoppotechnologyApplication.class, args);
+	}
+
+}
